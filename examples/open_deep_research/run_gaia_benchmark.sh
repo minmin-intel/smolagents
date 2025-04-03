66afffe6bfd8ffdc373dffb5b9f4dcf72b58f92b
minmin-intel <minmin.hou@intel.com>
2025-04-03 00:15:56 +0000

    run deepseek reasoner

--- examples/open_deep_research/run_gaia_benchmark.sh
+++ examples/open_deep_research/run_gaia_benchmark.sh
@@ -2,7 +2,7 @@ agent_type=code
 # agent_type=toolcalling
 
 PROVIDER="deepseek"
-MODEL="deepseek-chat"
+MODEL="deepseek-reasoner"
 
 # PROVIDER="openai"
 # MODEL="openai/gpt-4o"
@@ -14,8 +14,8 @@ subset=validation
 datapath=$WORKDIR/owl/examples/data/gaia/2023/
 export DATAPATH=${datapath}/${subset}
 test_type=text
-output=$WORKDIR/datasets/gaia/smolagents_tests/results_${test_type}_${PROVIDER}_singlecodeagent_${agent_type}.jsonl
-logfile=$WORKDIR/datasets/gaia/smolagents_tests/log_${test_type}_${PROVIDER}_singlecodeagent_${agent_type}.log
+output=$WORKDIR/datasets/gaia/smolagents_tests/results_${test_type}_${MODEL}_singlecodeagent.jsonl
+logfile=$WORKDIR/datasets/gaia/smolagents_tests/log_${test_type}_${MODEL}_singlecodeagent.log
 
 python test_web_agent.py \
 --provider $PROVIDER \
@@ -26,5 +26,6 @@ python test_web_agent.py \
 --answer_file $output \
 --subset $subset \
 --sampled \
---test_type $test_type | tee $logfile
+--test_type $test_type \
+--question 3 | tee $logfile
 #--debug | tee $logfile
